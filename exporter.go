b5ed629d22a93ce39b14717ce47a4273a69d4877
Jorge Luis Betancourt Gonzalez <jorge-luis.betancourt@trivago.com>
2020-01-30 16:27:58 +0100

    Modify for maintaining an internal fork

---
+++ exporter.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"sync"
+	"time"
+
+	_ "net/http/pprof"
+
+	"github.com/Shopify/sarama"
+	kazoo "github.com/krallistic/kazoo-go"
+	"github.com/prometheus/client_golang/prometheus"
+	plog "github.com/prometheus/common/log"
+)
+
+const (
+	namespace = "kafka"
+	clientID  = "kafka_exporter"
+)
+
+var (
+	clusterBrokers                     *prometheus.Desc
+	topicPartitions                    *prometheus.Desc
+	topicCurrentOffset                 *prometheus.Desc
+	topicOldestOffset                  *prometheus.Desc
+	topicPartitionLeader               *prometheus.Desc
+	topicPartitionReplicas             *prometheus.Desc
+	topicPartitionInSyncReplicas       *prometheus.Desc
+	topicPartitionUsesPreferredReplica *prometheus.Desc
+	topicUnderReplicatedPartition      *prometheus.Desc
+	consumergroupCurrentOffset         *prometheus.Desc
+	consumergroupCurrentOffsetSum      *prometheus.Desc
+	consumergroupLag                   *prometheus.Desc
+	consumergroupLagSum                *prometheus.Desc
+	consumergroupLagZookeeper          *prometheus.Desc
+	consumergroupMembers               *prometheus.Desc
+)
+
+// Exporter collects Kafka stats from the given server and exports them using
+// the prometheus metrics package.
+type Exporter struct {
+	client                  sarama.Client
+	clientPool              chan sarama.Client
+	topicFilter             *regexp.Regexp
+	groupFilter             *regexp.Regexp
+	mu                      sync.Mutex
+	useZooKeeperLag         bool
+	zookeeperClient         *kazoo.Kazoo
+	nextMetadataRefresh     time.Time
+	metadataRefreshInterval time.Duration
+	filterMode              bool
+}
+
+type kafkaOpts struct {
+	uri                      []string
+	useSASL                  bool
+	useSASLHandshake         bool
+	saslUsername             string
+	saslPassword             string
+	useTLS                   bool
+	tlsCAFile                string
+	tlsCertFile              string
+	tlsKeyFile               string
+	tlsInsecureSkipTLSVerify bool
+	kafkaVersion             string
+	useZooKeeperLag          bool
+	uriZookeeper             []string
+	zkRoot                   string
+	labels                   string
+	metadataRefreshInterval  string
+	clientPoolSize           int
+}
+
+// CanReadCertAndKey returns true if the certificate and key files already exists,
+// otherwise returns false. If lost one of cert and key, returns error.
+func CanReadCertAndKey(certPath, keyPath string) (bool, error) {
+	certReadable := canReadFile(certPath)
+	keyReadable := canReadFile(keyPath)
+
+	if certReadable == false && keyReadable == false {
+		return false, nil
+	}
+
+	if certReadable == false {
+		return false, fmt.Errorf("error reading %s, certificate and key must be supplied as a pair", certPath)
+	}
+
+	if keyReadable == false {
+		return false, fmt.Errorf("error reading %s, certificate and key must be supplied as a pair", keyPath)
+	}
+
+	return true, nil
+}
+
+// If the file represented by path exists and
+// readable, returns true otherwise returns false.
+func canReadFile(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+
+	defer f.Close()
+
+	return true
+}
+
+// NewExporter returns an initialized Exporter.
+func NewExporter(opts kafkaOpts, topicFilter string, groupFilter string, filterMode bool) (*Exporter, error) {
+	var zookeeperClient *kazoo.Kazoo
+	config := sarama.NewConfig()
+	config.ClientID = clientID
+	kafkaVersion, err := sarama.ParseKafkaVersion(opts.kafkaVersion)
+	if err != nil {
+		return nil, err
+	}
+	config.Version = kafkaVersion
+
+	if opts.useSASL {
+		config.Net.SASL.Enable = true
+		config.Net.SASL.Handshake = opts.useSASLHandshake
+
+		if opts.saslUsername != "" {
+			config.Net.SASL.User = opts.saslUsername
+		}
+
+		if opts.saslPassword != "" {
+			config.Net.SASL.Password = opts.saslPassword
+		}
+	}
+
+	if opts.useTLS {
+		config.Net.TLS.Enable = true
+
+		config.Net.TLS.Config = &tls.Config{
+			RootCAs:            x509.NewCertPool(),
+			InsecureSkipVerify: opts.tlsInsecureSkipTLSVerify,
+		}
+
+		if opts.tlsCAFile != "" {
+			if ca, err := ioutil.ReadFile(opts.tlsCAFile); err == nil {
+				config.Net.TLS.Config.RootCAs.AppendCertsFromPEM(ca)
+			} else {
+				plog.Fatalln(err)
+			}
+		}
+
+		canReadCertAndKey, err := CanReadCertAndKey(opts.tlsCertFile, opts.tlsKeyFile)
+		if err != nil {
+			plog.Fatalln(err)
+		}
+		if canReadCertAndKey {
+			cert, err := tls.LoadX509KeyPair(opts.tlsCertFile, opts.tlsKeyFile)
+			if err == nil {
+				config.Net.TLS.Config.Certificates = []tls.Certificate{cert}
+			} else {
+				plog.Fatalln(err)
+			}
+		}
+	}
+
+	if opts.useZooKeeperLag {
+		zkURI := kazoo.BuildConnectionStringWithChroot(opts.uriZookeeper, opts.zkRoot)
+		plog.Infof("Zookeper URI: %s", zkURI)
+		zookeeperClient, err = kazoo.NewKazooFromConnectionString(zkURI, nil)
+	}
+
+	interval, err := time.ParseDuration(opts.metadataRefreshInterval)
+	if err != nil {
+		plog.Errorln("Cannot parse metadata refresh interval")
+		panic(err)
+	}
+
+	config.Metadata.RefreshFrequency = interval
+
+	if opts.clientPoolSize < 1 {
+		opts.clientPoolSize = 1
+	}
+
+	client, err := sarama.NewClient(opts.uri, config)
+
+	if err != nil {
+		plog.Errorln("Error Init Kafka Client")
+		panic(err)
+	}
+
+	clientChan := make(chan sarama.Client, opts.clientPoolSize)
+	for i := 0; i < opts.clientPoolSize; i++ {
+		plog.Infof("ClientPool: %d", i)
+		c, err := sarama.NewClient(opts.uri, config)
+		if err != nil {
+			plog.Errorf("Failed to start client pool connection: %v", err)
+			continue
+		}
+		clientChan <- c
+	}
+
+	plog.Infoln("Done Init Clients")
+
+	// Init our exporter.
+	return &Exporter{
+		client:                  client,
+		clientPool:              clientChan,
+		topicFilter:             regexp.MustCompile(topicFilter),
+		groupFilter:             regexp.MustCompile(groupFilter),
+		useZooKeeperLag:         opts.useZooKeeperLag,
+		zookeeperClient:         zookeeperClient,
+		nextMetadataRefresh:     time.Now(),
+		metadataRefreshInterval: interval,
+		filterMode:              filterMode,
+	}, nil
+}
---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/danielqsj/kafka_exporter
+
+require (
+	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/Shopify/sarama v1.26.0
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/google/go-github/v25 v25.1.3 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/krallistic/kazoo-go v0.0.0-20170526135507-a15279744f4e
+	github.com/pierrec/lz4 v2.4.1+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/profile v1.2.1 // indirect
+	github.com/prometheus/client_golang v1.4.0
+	github.com/prometheus/common v0.9.1
+	github.com/prometheus/promu v0.5.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20190826022208-cac0b30c2563
+	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da // indirect
+	golang.org/x/crypto v0.0.0-20200128174031-69ecbb4d6d5d // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/jcmturner/gokrb5.v7 v7.4.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
+
+go 1.13
